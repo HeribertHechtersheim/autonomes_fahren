36e309cb83e130e93578a5de8f43bd0b090a798d
Jonastuhh <eckhoff-jonas@web.de>
2019-01-27 23:36:54 +0100

    Update birdeyetransform.m

--- Aufgabe 3/birdeyetransform.m
+++ Aufgabe 3/birdeyetransform.m
@@ -1,23 +1,26 @@
 function [B] = birdeyetransform(I)
 
 % Describe camera configuration
+[n,m]=size(I);
     load('picam_params.mat');
-    %focalLength    =  picam_params.FocalLength; % [fx, fy]
-    %principalPoint =  picam_params.PrincipalPoint; % [cx, cy]
-    %imageSize      = picam_params.ImageSize;           % [mrows, ncols]
+    imageSize      = picam_params.ImageSize;           % [mrows, ncols]
+    focalLength    =  picam_params.FocalLength/imageSize(2)*m; % [fx, fy]
+    principalPoint =  picam_params.PrincipalPoint/imageSize(2)*m; % [cx, cy]
+    imageSize=[m,n];
+    
     height         = 0.263;               % height in meters from the ground
     pitch          = 14;                   % pitch of the camera, towards the ground, in degrees
  
-    %camIntrinsics = cameraIntrinsics(focalLength, principalPoint, imageSize);
-    sensor = monoCamera(picam_params, height, 'Pitch', pitch);
+    camIntrinsics = cameraIntrinsics(focalLength, principalPoint, imageSize);
+    sensor = monoCamera(camIntrinsics, height, 'Pitch', pitch);
     
  
     % Define bird's-eye-view transformation parameters
     distAheadOfSensor = 1; % in meters
-    spaceToOneSide    = 0.50;  
-    bottomOffset      = 0.35;  
+    spaceToOneSide    = 0.40;  
+    bottomOffset      = 0.4;  
     outView = [bottomOffset, distAheadOfSensor, -spaceToOneSide, spaceToOneSide];
-    outImageSize = [NaN, 800]; % output image width in pixels
+    outImageSize = [NaN, 320]; % output image width in pixels
  
     birdsEyeConfig = birdsEyeView(sensor, outView, outImageSize);
  
